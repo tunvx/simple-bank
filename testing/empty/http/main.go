8c5a62531614904196230b59fe7677e2cea76350
tunvx <tunv.todo@gmail.com>
2025-03-07 14:58:39 +0700

    [update] small

--- testing/empty/http/main.go
+++ testing/empty/http/main.go
@@ -6,20 +6,20 @@ import (
 )
 
 func main() {
-	// Define the handler for GET /v1/get_empty_connection
-	http.HandleFunc("/v1/get_empty_connection", func(w http.ResponseWriter, r *http.Request) {
+	// Define the handler for GET /v1/test/empty_get
+	http.HandleFunc("/v1/test/empty_get", func(w http.ResponseWriter, r *http.Request) {
 		// Set the response header to JSON
 		w.Header().Set("Content-Type", "application/json")
 
 		// Create the response data
 		response := map[string]string{
-			"message": "Connection is empty",
+			"message": "Ok",
 		}
 
 		// Encode the response as JSON and send it
 		json.NewEncoder(w).Encode(response)
 	})
 
-	// Run the server on port 8888
-	http.ListenAndServe(":8888", nil)
+	// Run the server on port 8083
+	http.ListenAndServe(":8083", nil)
 }
